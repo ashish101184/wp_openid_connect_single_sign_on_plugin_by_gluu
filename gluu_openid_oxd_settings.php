16e2f0a0902c70c049d53ea652e39e02a45e604c
dollar007 <vlad.karapetyan.1988@mail.ru>
2016-09-17 20:24:57 +0400

    deleting all plugin optinons, after deactivating or uninstalling

--- gluu_openid_oxd_settings.php
+++ gluu_openid_oxd_settings.php
@@ -82,18 +82,19 @@ class gluu_OpenID_OXD {
 		add_option('gluu_oxd_openid_custom_scripts',array('none'));
 	}
 	function gluu_oxd_openid_deactivate() {
-		delete_option('gluu_oxd_openid_new_registration');
-		delete_option('gluu_oxd_openid_message');
-		delete_option('gluu_op_host');
-		delete_option('gluu_redirect_url');
-		delete_option('gluu_oxd_openid_scops');
-		delete_option('gluu_oxd_openid_custom_scripts');
-		delete_option('gluu_Oxd_Activated_Plugin');
-		delete_option('gluu_oxd_openid_admin_email');
 		delete_option('gluu_oxd_config');
 		delete_option('gluu_oxd_id');
+		delete_option('gluu_oxd_openid_new_registration');
+		delete_option('gluu_oxd_openid_admin_email');
+		delete_option('gluu_oxd_openid_message');
+		delete_option('gluu_oxd_openid_scops');
 		delete_option('gluu_auth_type');
 		delete_option('gluu_send_user_check');
+		delete_option('gluu_custom_url');
+		delete_option('gluu_op_host');
+		delete_option('gluu_redirect_url');
+		delete_option('gluu_oxd_openid_custom_scripts');
+		delete_option('gluu_Oxd_Activated_Plugin');
 	}
 	function gluu_oxd_openid_activate() {
 		add_option('gluu_Oxd_Activated_Plugin','Plugin-Slug');
@@ -926,6 +927,10 @@ class gluu_OpenID_OXD {
 		delete_option('gluu_auth_type');
 		delete_option('gluu_send_user_check');
 		delete_option('gluu_custom_url');
+		delete_option('gluu_op_host');
+		delete_option('gluu_redirect_url');
+		delete_option('gluu_oxd_openid_custom_scripts');
+		delete_option('gluu_Oxd_Activated_Plugin');
 	}
 	function gluu_oxd_openid_end_session() {
 		session_start();
--- uninstall.php
+++ uninstall.php
@@ -12,6 +12,11 @@ if(is_admin()){
 	delete_option('gluu_oxd_openid_scops');
 	delete_option('gluu_auth_type');
 	delete_option('gluu_send_user_check');
+	delete_option('gluu_custom_url');
+	delete_option('gluu_op_host');
+	delete_option('gluu_redirect_url');
+	delete_option('gluu_oxd_openid_custom_scripts');
+	delete_option('gluu_Oxd_Activated_Plugin');
 }
 
 ?>
